b35f5d166ed0630c23f63578865e3432a63a12e7
Gavin Thomas <gavinlthomas05@gmail.com>
2026-04-09 18:28:34 +0100

    commit: got app running in emulator + cleaned up boiler plate code stopping it from working

--- app/src/main/java/com/example/ridesignal/signalPad.kt
+++ app/src/main/java/com/example/ridesignal/signalPad.kt
@@ -20,22 +20,8 @@ class signalPad : AppCompatActivity() {
         binding = ActivitySignalPadBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_signal_pad)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        binding.btnOptions.setOnClickListener {
+            // This is where you will trigger the "Leave Session" menu later
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_signal_pad)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
